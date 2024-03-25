3fd25f5dd997b8f57e269efbb4d4c62b01dd3226
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-03-25 13:27:53 +0700

    fix: responsive and add: vector

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import Image from "./assets/bg-cafe.jpg";
+import Vector from "./assets/vector.svg";
 import { Button, CoffeeItem } from "./components";
 
 const App = () => {
@@ -39,15 +40,25 @@ const App = () => {
       />
 
       {/* Container */}
-      <div className="m-4 md:m-0 md:max-w-screen-lg p-4 sm:p-8 md:p-12 flex flex-col items-center gap-4 bg-[#111315] rounded-lg z-10">
-        <h2 className="text-xl md:text-3xl font-semibold text-[#FEF7EE]">
-          Our Collection
-        </h2>
-        <p className="text-sm md:text-base text-[#6F757C] md:w-2/4 text-center">
-          Introducing our Coffee Collection, a selection of unique coffees from
-          different roast types and origins, expertly roasted in small batches
-          and shipped fresh weekly.
-        </p>
+      <div className="relative w-11/12 sm:w-3/4 xl:max-w-screen-lg p-4 sm:p-8 md:p-16 flex flex-col items-center gap-4 bg-[#111315] rounded-lg overflow-hidden z-10">
+        <div className="relative lg:w-3/4 xl:w-2/4 text-center">
+          <img
+            src={Vector}
+            alt="vector"
+            className="absolute bottom-0 -right-1/4 sm:right-0 -z-10"
+          />
+
+          <div className="space-y-4 z-20">
+            <h2 className="text-xl md:text-3xl font-semibold text-[#FEF7EE]">
+              Our Collection
+            </h2>
+            <p className="text-sm md:text-base text-[#6F757C]">
+              Introducing our Coffee Collection, a selection of unique coffees
+              from different roast types and origins, expertly roasted in small
+              batches and shipped fresh weekly.
+            </p>
+          </div>
+        </div>
         <div className="flex items-center gap-4">
           <Button
             isActive={filterBy === "all"}
@@ -62,7 +73,7 @@ const App = () => {
             Available Now
           </Button>
         </div>
-        <div className="mt-2 grid grid-cols-1 sm:grid-cols-3 gap-12 sm:gap-x-8 sm:gap-y-16">
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 xl:gap-x-8 xl:gap-y-16">
           {coffee.map((coffee, index) => {
             return <CoffeeItem data={coffee} key={index} />;
           })}
